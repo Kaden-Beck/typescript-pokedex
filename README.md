95d790b80fb0595c4ab20d57998f463b38e00d67
Kaden-Beck <kadenb12@gmail.com>
2026-03-01 14:51:09 -0700

    Implemented basic CLI REPL

---
+++ README.md
@@ -0,0 +1,4 @@
+# Overview
+
+This is a practice project provided by Boot.dev to practice using TypeScript and test driven development using Vitest. 
+
--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,6 @@
+import * as readline from 'readline';
+import { callbackify } from 'util';
+
 export function cleanInput(input: string): string[] {
     let cleanOutput: string[] = input.trim().split(/\s+/);
 
@@ -7,3 +10,31 @@ export function cleanInput(input: string): string[] {
     
     return cleanOutput;
 }
+
+export function startREPL() : void {
+    // Set CLI prompt
+    let textPrompt = "Pokedex >";
+    
+    // Initialize line reader
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout, 
+        prompt: textPrompt,
+    });
+
+    // Display interface prompt
+    rl.prompt();
+
+    // Listen for input
+    rl.on("line", (input) => {
+        
+        // if input is not empty clean input
+        if (input.trim() !== "") {
+            let cleanOutput: string[] = cleanInput(input);
+            console.log(`Your command was: ${cleanOutput[0]}`)
+        } 
+            rl.prompt();
+    })
+}
+
+
